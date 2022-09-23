bd7668b7c3f29a43107a0293ab72fd81955671c9
Siddhartha <sid@countvajhula.com>
2022-09-22 20:29:41 -0700

    Improve the time estimation method by using dynamic-require
    Also avoid regex parsing by using time-apply, and support relative
    vs collection paths via a -f flag.

--- require-latency/require-latency.rkt
+++ require-latency/require-latency.rkt
@@ -1,17 +1,15 @@
 #!/usr/bin/env racket
 #lang cli
 
-(provide time-racket
-         time-module-ms)
+(provide time-module-ms)
 
 (require racket/port
-         racket/format)
+         racket/format
+         racket/string)
 
 #|
-This works by:
-1. Running `racket -l <module_name>` and `racket -l racket/base` independently
-2. Subtracting the latter from the former.
-3. Printing that result in milliseconds.
+This uses the following method to estimate the require latency:
+  racket -l racket/base -e "(time (dynamic-require <module_name> 0))"
 
 where <module_name> is the argument you specified at the command line,
 e.g. ./require-latency.rkt racket/list
@@ -20,32 +18,44 @@ The idea is to subtract out the contribution from racket/base, so that
 what remains is just the time contributed by requiring the specified module.
 
 Methodology based on discussion with Grzegorz Szansa, Sam Tobin-Hochstadt,
-and others.
+Michael Ballantyne, and others.
 |#
 
-(define (time-racket [module-name "racket/base"])
+(define (time-command-absolute module-name)
+  (~a "(let () (define-values (_0 _1 ms _2) " "(time-apply (lambda () (dynamic-require " "'" module-name " 0)) null)) ms)"))
+
+(define (time-command-relative module-name)
+  (~a "(let () (define-values (_0 _1 ms _2) " "(time-apply (lambda () (dynamic-require " "\"" module-name "\"" " 0)) null)) ms)"))
+
+(define (time-module-ms module-name)
   (define-values (sp out in err)
-    (subprocess #f #f #f (find-executable-path "time") "-p" (find-executable-path "racket") "-l" module-name))
-  (define result (port->string err))
-  (define seconds (string->number
-                   (car
-                    (regexp-match #px"[\\d|\\.]+"
-                                  (car
-                                   (regexp-match #rx"(?m:^real.*)"
-                                                 result))))))
+    (subprocess #f
+                #f
+                #f
+                (find-executable-path "racket")
+                "-l"
+                "racket/base"
+                "-e"
+                ((if (file)
+                     time-command-relative
+                     time-command-absolute)
+                 module-name)))
+  (define ms (string->number (string-trim (port->string out))))
   (close-input-port out)
   (close-output-port in)
   (close-input-port err)
   (subprocess-wait sp)
-  seconds)
+  ms)
 
-(define (time-module-ms module-name)
-  (* 1000
-     (- (time-racket module-name)
-        (time-racket))))
+(flag (file)
+  ("-f" "--file" "Treat the input as a relative path to a file instead of an installed module in a collection.")
+  (file #t))
 
 (program (require-latency [module-name "module path"])
-  (displayln (~a (time-module-ms module-name) " ms")))
+  (let ([result (time-module-ms module-name)])
+    (if result
+        (printf (~a result " ms\n"))
+        (fprintf (current-error-port) "Module not found!\n"))))
 
 (module+ main
   (run require-latency))
